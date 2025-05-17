0877616ad1a6fb5914d376a0e46b5d97135941d5
Ric <ric@brighteraccounting.com>
2025-05-17 09:55:58 +0100

    Fix duplicate model directive

---
+++ XeroNetStandardApp/Models/HomeIndexViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace XeroNetStandardApp.Models
+{
+    public class HomeIndexViewModel
+    {
+        public bool IsConnected { get; set; }
+        public List<OrgTenant> Tenants { get; set; } = new();
+    }
+}

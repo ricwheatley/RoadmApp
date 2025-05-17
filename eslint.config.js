e42971a181231724148a7514d144660407f74f56
Ric <ric@brighteraccounting.com>
2025-05-17 10:53:54 +0100

    Add ESLint configuration and lint step

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module'
+    },
+    plugins: {},
+    rules: {
+    }
+  }
+];
+

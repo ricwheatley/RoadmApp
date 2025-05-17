2f452784b3a66da985fd8f1879fe0bfdf60a8c1d
Ric <ric@brighteraccounting.com>
2025-05-17 22:29:11 +0100

    codex/mirror-data-collection-process-for-assets-api

--- XeroNetStandardApp.Tests/PollingServiceTests.cs
+++ XeroNetStandardApp.Tests/PollingServiceTests.cs
@@ -1,7 +1,7 @@
-using System.Threading.Tasks;
 using System;
-using Microsoft.Extensions.Logging.Abstractions;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Logging.Abstractions;
 using Xero.NetStandard.OAuth2.Token;
 using Xunit;
 using XeroNetStandardApp.Services;
@@ -37,8 +37,16 @@ namespace XeroNetStandardApp.Tests
         {
             var raw = new StubRaw();
             var assets = new StubAssets();
-            var tokenService = new TokenService(new NullDataProtectionProvider());
-            tokenService.StoreToken(new XeroOAuth2Token { AccessToken = "t", ExpiresAtUtc = DateTime.UtcNow.AddHours(1) });
+
+            // Ephemeral provider stores keys only in memory – perfect for unit tests
+            var dataProtectionProvider = new EphemeralDataProtectionProvider();
+            var tokenService = new TokenService(dataProtectionProvider);
+
+            tokenService.StoreToken(new XeroOAuth2Token
+            {
+                AccessToken = "t",
+                ExpiresAtUtc = DateTime.UtcNow.AddHours(1)
+            });
 
             var svc = new PollingService(tokenService, raw, assets, NullLogger<PollingService>.Instance);
             await svc.RunEndpointAsync("123", "assets");

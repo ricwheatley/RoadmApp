53b49fbad9e3f7a1a894fb02cb86c093b01b9722
Ric <ric@brighteraccounting.com>
2025-05-20 07:25:40 +0100

    Update PollingStats.cs

--- XeroNetStandardApp/Models/PollingStats.cs
+++ XeroNetStandardApp/Models/PollingStats.cs
@@ -4,7 +4,7 @@ namespace XeroNetStandardApp.Models
     {
         public System.Guid OrganisationId { get; set; }
         public System.DateTimeOffset LastCall { get; set; }
-        public bool EndpointsSuccess { get; set; }
+        public int EndpointsSuccess { get; set; }
         public int EndpointsFail { get; set; }
         public int RecordsInserted { get; set; }
     }
